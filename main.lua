70dfcaae5693674f3dce39fb0f28400773ad6dbc
Joshua Sigona <sigonasr2@gmail.com>
2021-06-12 18:39:47 +0900

    GPS locator

--- main.lua
+++ main.lua
@@ -4,35 +4,29 @@
 --
 -----------------------------------------------------------------------------------------
 
-local latitude = display.newText( "-", 100, 50, native.systemFont, 16 )
-local longitude = display.newText( "-", 100, 100, native.systemFont, 16 )
-local altitude = display.newText( "-", 100, 150, native.systemFont, 16 )
-local accuracy = display.newText( "-", 100, 200, native.systemFont, 16 )
-local speed = display.newText( "-", 100, 250, native.systemFont, 16 )
-local direction = display.newText( "-", 100, 300, native.systemFont, 16 )
-local time = display.newText( "-", 100, 350, native.systemFont, 16 )
- 
-local locationHandler = function( event )
-    if ( event.errorCode ) then
-        native.showAlert( "GPS Location Error", event.errorMessage, {"OK"} )
-        print( "Location error: " .. tostring( event.errorMessage ) )
-    else
-        local latitudeText = string.format( '%.4f', event.latitude )
-        latitude.text = latitudeText
-        local longitudeText = string.format( '%.4f', event.longitude )
-        longitude.text = longitudeText
-        local altitudeText = string.format( '%.3f', event.altitude )
-        altitude.text = altitudeText
-        local accuracyText = string.format( '%.3f', event.accuracy )
-        accuracy.text = accuracyText
-        local speedText = string.format( '%.3f', event.speed )
-        speed.text = speedText
-        local directionText = string.format( '%.3f', event.direction )
-        direction.text = directionText
-        local timeText = string.format( '%.0f', event.time )
-        time.text = timeText
-    end
+local currentLocation = nil
+local myMap = native.newMapView( 20, 20, 280, 360 )
+
+local updateMap = function(event)
+	-- Create a native map view
+	if (myMap==nil) then
+		currentLocation = {latitude=-1,longitude=-1}
+	else 
+		myMap.x = display.contentCenterX
+		currentLocation = myMap:getUserLocation()
+		myMap:setCenter( currentLocation.latitude, currentLocation.longitude )
+	end
+
+	local locationText = display.newText( "Location: ", 0, 400, native.systemFont, 16 )
+	locationText.anchorY = 0
+	locationText.x = display.contentCenterX
+	if ( currentLocation.errorCode or ( currentLocation.latitude == 0 and currentLocation.longitude == 0 ) ) then
+		locationText.text = currentLocation.errorMessage
+	else
+		locationText.text = "Current location: " .. currentLocation.latitude .. "," .. currentLocation.longitude
+		-- myMap:addMarker( currentLocation.latitude, currentLocation.longitude )
+	end
 end
-  
--- Activate location listener
-Runtime:addEventListener( "location", locationHandler )
+
+		 
+timer.performWithDelay( 1000, updateMap, -1)
